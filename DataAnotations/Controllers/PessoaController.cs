b0d7e1ac11a50ff481654b465741c5443d0d5c9a
EricoSC <ericoscamargo@gmail.com>
2019-07-14 10:21:22 -0300

    Add project files.

---
+++ DataAnotations/Controllers/PessoaController.cs
@@ -0,0 +1,54 @@
+﻿using DataAnotations.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataAnotations.Controllers
+{
+    public class PessoaController : Controller
+    {
+        // GET: Pessoa
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            //if (string.IsNullOrEmpty(pessoa.Nome))
+            //{
+            //    ModelState.AddModelError("Nome", "O Campo Nome é Obrigatório");
+            //}
+            //if (pessoa.Senha != pessoa.ConfirmarSenha) 
+            //{
+            //    ModelState.AddModelError("", "As Senhas não Coferem");
+            //}
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+            return View(pessoa);
+        }
+
+        public ActionResult Resultado(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+
+        public ActionResult LoginUnico(string login)
+        {
+            var bancoDeNomesDeExemplo = new Collection<string>
+            {
+                "Cleyton",
+                "Anderson",
+                "Renata"
+            };
+            return Json(bancoDeNomesDeExemplo.All(c => c.ToLower() != login.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DataAnotations/Models/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataAnotations.Models
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage ="Nome deve ser preenchido")]
+        public  string Nome { get; set; }
+        [StringLength(50,MinimumLength = 5,ErrorMessage ="A Observação deve ter de 5 a 50 caracteres")]
+        [Required(ErrorMessage ="Obrigatório a informação da Idade")]
+        public string Observacao{ get; set; }
+        [Range(18,50,ErrorMessage ="Minimo de 18 e maximo de 50 anos")]
+        public int Idade { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*", ErrorMessage ="E-Mail com formato invalido")]
+        public string Email{ get; set; }
+        [RegularExpression(@"[a-zA-Z]{5,15}",ErrorMessage ="Login deve possuir letraz e 5 a 15 caracteres")]
+        [Required(ErrorMessage = "Deve ser preenchido")]
+        [Remote("LoginUnico","Pessoa",ErrorMessage ="Este Nome de Login Já existe")]
+        public string Login { get; set; }
+        [Required(ErrorMessage ="A Senha deve ser informada")]
+        public string  Senha { get; set; }
+        [System.ComponentModel.DataAnnotations.Compare("Senha", ErrorMessage ="As Senhas não conferem")]
+        public string ConfirmarSenha { get; set; }
+    }
+}
